29f6065f639d09dc1582b5fc8e232971cc3baefb
Daniel Capeans <dcapeans@gmail.com>
2021-06-17 01:58:33 -0300

    post games route

--- src/app.js
+++ src/app.js
@@ -44,6 +44,46 @@ app.post("/categories", async (req, res) => {
     }
 })
 
+app.get("/games", async (req, res) => {
+    const nameQuery = req.query.name ?? "";
+
+    try {
+        const games = await connection.query('SELECT * FROM games WHERE name ILIKE $1', [nameQuery + "%"])
+        res.send(games.rows)
+    } catch (error) {
+        console.log(error)
+        res.send("deu erro")
+    }
+})
+
+app.post("/games", async (req, res) => {
+    const { name, image, stockTotal, categoryId, pricePerDay } = req.body
+    const nameIsEmpty = name.length === 0
+    const stockAndPricePositives = (stockTotal > 0 && pricePerDay > 0)
+    const categories = await connection.query('SELECT * FROM categories')
+    const categoryIdExists = categories.rows.find(category => category.id === categoryId)
+
+    const games = await connection.query('SELECT * FROM games')
+    const gameNameExists = games.rows.find(game => game.name === name)
+
+    if(nameIsEmpty || stockAndPricePositives || !categoryIdExists){
+        res.send("deu ruim")
+
+        return
+    } else if(gameNameExists){
+        res.sendStatus(409)
+        return
+    }
+
+    try {
+        await connection.query('INSERT INTO games (name, image, stockTotal, categoryId, pricePerDay) VALUES ($1, $2, $3, $4, $5)', [name, image, stockTotal, categoryId, pricePerDay])
+        res.sendStatus(201)
+    } catch (error) {
+        console.log(error)
+        res.send("deu ruim")
+    }
+})
+
 app.listen(4000, () => {
     console.log("Server listening at port 4000")
 })
