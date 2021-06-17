926d6a6be80a6d63077531dc5f31220916c45b29
Daniel Capeans <dcapeans@gmail.com>
2021-06-17 01:05:23 -0300

    get categories route

--- src/app.js
+++ src/app.js
@@ -14,6 +14,16 @@ const connection = new Pool({
     database: 'boardcamp'
 })
 
+app.length("/categories", async(req, res) => {
+    try {
+        const categories = await connection.query('SELECT * FROM categories')
+        res.send(categories.rows)
+    } catch (error) {
+        console.log(error)
+        res.send("deu ruim")
+    }
+})
+
 
 
 
