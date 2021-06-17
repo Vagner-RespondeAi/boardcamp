101b9af40da626006c4172afec49ae82cee51ef3
Daniel Capeans <dcapeans@gmail.com>
2021-06-17 18:28:25 -0300

    customers get routes

--- src/app.js
+++ src/app.js
@@ -1,26 +1,16 @@
-import pg from 'pg'
 import express from 'express'
+import connection from './database/database.js'
 
 const app = express()
 app.use(express.json())
 
-const { Pool } = pg
-
-const connection = new Pool({
-    user: 'bootcamp_role',
-    password: 'senha_super_hiper_ultra_secreta_do_role_do_bootcamp',
-    host: 'localhost',
-    port: 5432,
-    database: 'boardcamp'
-})
-
+// CRUD CATEGORIES
 app.get("/categories", async (req, res) => {
     try {
         const categories = await connection.query('SELECT * FROM categories')
         res.send(categories.rows)
     } catch (error) {
-        console.log(error)
-        res.send("deu ruim")
+        res.sendStatus(400)
     }
 })
 
@@ -39,49 +29,86 @@ app.post("/categories", async (req, res) => {
         await connection.query('INSERT INTO categories (name) VALUES ($1)', [name])
         res.send("OK")
     } catch (error) {
-        console.log(error)
         res.sendStatus(201)
     }
 })
 
+// CRUD GAMES
 app.get("/games", async (req, res) => {
     const nameQuery = req.query.name ?? "";
-
     try {
-        const games = await connection.query('SELECT * FROM games WHERE name ILIKE $1', [nameQuery + "%"])
+        const games = await connection.query(`
+            SELECT games.*, categories.name AS "categoryName" 
+            FROM games JOIN categories ON games."categoryId" = categories.id
+            WHERE games.name ILIKE $1
+            `, [nameQuery + "%"]
+        )
         res.send(games.rows)
     } catch (error) {
-        console.log(error)
-        res.send("deu erro")
+        res.sendStatus(400)
     }
 })
 
 app.post("/games", async (req, res) => {
-    const { name, image, stockTotal, categoryId, pricePerDay } = req.body
-    const nameIsEmpty = name.length === 0
-    const stockAndPricePositives = (stockTotal > 0 && pricePerDay > 0)
-    const categories = await connection.query('SELECT * FROM categories')
-    const categoryIdExists = categories.rows.find(category => category.id === categoryId)
-
-    const games = await connection.query('SELECT * FROM games')
-    const gameNameExists = games.rows.find(game => game.name === name)
-
-    if(nameIsEmpty || !stockAndPricePositives || !categoryIdExists){
-        res.sendStatus(400)
-        return
-    } else if(gameNameExists){
-        res.sendStatus(409)
-        return
-    }
-
     try {
+        const { name, image, stockTotal, categoryId, pricePerDay } = req.body
+        const nameIsEmpty = name.length === 0
+        const stockAndPricePositives = (stockTotal > 0 && pricePerDay > 0)
+        const categories = await connection.query('SELECT * FROM categories')
+        const categoryIdExists = categories.rows.find(category => category.id === categoryId)
+
+        const games = await connection.query('SELECT * FROM games')
+        const gameNameExists = games.rows.find(game => game.name === name)
+
+        if(nameIsEmpty || !stockAndPricePositives || !categoryIdExists){
+            res.sendStatus(400)
+            return
+        } else if(gameNameExists){
+            res.sendStatus(409)
+            return
+        }
+
         await connection.query('INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5)', [name, image, stockTotal, categoryId, pricePerDay])
         res.sendStatus(201)
     } catch (error) {
-        console.log(error)
-        res.send("deu ruim")
+        res.sendStatus(400)
     }
 })
+
+//CRUD CUSTOMERS
+app.get("/customers", async (req, res) => {
+    const cpfQuery =  req.query.cpf ?? ""
+    try {
+        const customers = await connection.query(`
+        SELECT * 
+        FROM customers
+        WHERE cpf ILIKE $1
+        `, [cpfQuery + "%"])
+
+        res.send(customers.rows)
+    } catch (error) {
+        res.sendStatus(400)
+    }
+})
+
+app.get("/customers/:id", async (req, res) => {
+    try {
+        const customers = await connection.query(`
+        SELECT * 
+        FROM customers 
+        WHERE id ILIKE $1
+        `, [req.params.id])
+        res.send(customers.rows)
+    } catch (error) {
+        res.sendStatus(404)
+    }
+})
+
+
+app.post("/customers", async (req, res) => {
+
+})
+
 
 app.listen(4000, () => {
     console.log("Server listening at port 4000")
